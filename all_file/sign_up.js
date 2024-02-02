34d017bd200fd8a1f6d922c43268f009875b35a1
Sajjad-Hussain220 <narejo220@gmail.com>
2024-02-02 23:55:20 +0500

    web_q_a

---
+++ all_file/sign_up.js
@@ -0,0 +1,12 @@
+function togglePassword(inputId) {
+    const passwordInput = document.getElementById(inputId);
+    const eyeIcon = passwordInput.nextElementSibling.querySelector('i');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+    eyeIcon.classList.toggle('ri-eye-off-fill');
+    eyeIcon.classList.toggle('ri-eye-fill');
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {
+    // Get the navbar element
+    var navbar = document.getElementById("nave_button");
+
+    // Get the button element
+    var searchButton = document.querySelector(".serach_nav");
+
+    // Check if the user has scrolled down enough to make the navbar sticky
+    if (window.pageYOffset >= navbar.offsetTop) {
+        // If scrolled down, display the search button
+        searchButton.style.display = "inline-block";
+    } else {
+        // If at the top, hide the search button
+        searchButton.style.display = "none";
+    }
+};
+
+// Handle window resize for responsiveness
+window.onresize = function() {
+    var searchButton = document.querySelector(".serach_nav");
+
+    // Display the search button if the window width is greater than a certain threshold (e.g., 600px)
+    if (window.innerWidth > 600) {
+        searchButton.style.display = "inline-block";
+    } else {
+        searchButton.style.display = "none";
+    }
+};
+
